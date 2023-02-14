40f07c4b499a40807216dcd41135f332a55909dc
IsaacNascimento <isaacs.nascimento17@gmail.com>
2023-02-13 21:33:39 -0300

    Morgan package - print/log api calls

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
 const planetsRouter = require('./routes/planets/planetsRouter');
@@ -8,6 +9,9 @@ const planetsRouter = require('./routes/planets/planetsRouter');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 

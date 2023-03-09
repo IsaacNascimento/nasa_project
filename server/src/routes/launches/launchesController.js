fcf0ad918c466c267b8e62e3cc352ba46fc2e096
IsaacNascimento <isaacs.nascimento17@gmail.com>
2023-03-08 21:47:17 -0300

    validation to create a new launch

--- server/src/routes/launches/launchesController.js
+++ server/src/routes/launches/launchesController.js
@@ -1,14 +1,35 @@
-const { gettAllLaunches, addNewLaunches } = require('../../models/launchesModel');
+const {
+  gettAllLaunches,
+  addNewLaunches,
+} = require("../../models/launchesModel");
 
 function httpGetAllLanches(req, res) {
-    return res.status(200).json(gettAllLaunches()) || function(){};
-};
+  return res.status(200).json(gettAllLaunches()) || function () {};
+}
 
 function httpAddNewLaunche(req, res) {
-    const launch = req.body;
-    launch.launchDate = new Date(launch.launchDate);
-    addNewLaunches(launch)
-    return res.status(201).json(launch)
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.destination ||
+    !launch.launchDate 
+  ) {
+    return res.status(400).json({
+        error: 'Missing required Launch property'
+    })
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+        error: 'Invalid launch date'
+    })
+  }
+
+  addNewLaunches(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = { httpGetAllLanches, httpAddNewLaunche };
